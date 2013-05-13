f30454c2c5f34d02c9a870f80d7340bdcdc2af8d
gilbert <gilbert@localhost.localdomain>
2013-05-13 15:39:19 +0200

    plugin rename problem fixed

--- lib/ewwiki_hook.rb
+++ lib/ewwiki_hook.rb
@@ -1,6 +1,6 @@
 class EWWikiHook < Redmine::Hook::ViewListener
   def view_layouts_base_html_head(context={})
-    javascript_include_tag("toolbarmod.js", :plugin => 'filebrowser') +
-      stylesheet_link_tag('toolbarmod.css', :plugin => 'filebrowser')
+    javascript_include_tag("toolbarmod.js", :plugin => 'redmine_repobrowser') +
+      stylesheet_link_tag('toolbarmod.css', :plugin => 'redmine_repobrowser')
   end
 end
