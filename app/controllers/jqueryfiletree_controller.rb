eae67ffa58cb667847de42fadd98870448c5e687
gilbert <gilbert@localhost.localdomain>
2013-05-13 18:23:23 +0200

    Error handling, if no repo is defined

--- app/controllers/jqueryfiletree_controller.rb
+++ app/controllers/jqueryfiletree_controller.rb
@@ -16,23 +16,27 @@ class JqueryfiletreeController < ApplicationController
     @project_id = @project.id
     @project = Project.find(@project_id)
     @repository = @project.repository
-    @dirs = []
-    @files = []
-    p = @parent
-    p.gsub!('%20',' ')
-    p.gsub!('\/\/','\/')
-    @entries = @repository.entries(path=p)
-    @entries.each do |entrie|
-      r = entrie.path
-      r = r.split('/').last
-      if entrie.is_dir?
-        @dirs.push(r)
-      else
-        @files.push(r)
+    if (@repository == nil)
+      render :text => "There is no repository defined for this project"
+    else
+      @dirs = []
+      @files = []
+      p = @parent
+      p.gsub!('%20',' ')
+      p.gsub!('\/\/','\/')
+      @entries = @repository.entries(path=p)
+      @entries.each do |entrie|
+        r = entrie.path
+        r = r.split('/').last
+        if entrie.is_dir?
+          @dirs.push(r)
+        else
+          @files.push(r)
+        end
       end
+      @dir = [@dirs,@files]
+      render :layout => 'reduced'
     end
-    @dir = [@dirs,@files]
-    render :layout => 'reduced'
   end
 
 end
