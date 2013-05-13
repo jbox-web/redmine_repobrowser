5ff564a8d015073f9e9cf99cbdddfd50f2bf8957
gilbert <gilbert@localhost.localdomain>
2013-05-13 16:49:34 +0200

    reduced complexity of file structure

--- app/controllers/jqueryfiletree_controller.rb
+++ app/controllers/jqueryfiletree_controller.rb
@@ -1,25 +1,34 @@
 class JqueryfiletreeController < ApplicationController
   protect_from_forgery :only => []
+
+  before_filter :find_project
+
+  def find_project
+    @project = Project.find(params[:id])
+  end
+
   def content
     @parent = params[:dir]
-    @project = Project.find(1)
+    @project_id = @project.id
+    @project = Project.find(@project_id)
     @repository = @project.repository
-@dirs = []
-@files = []
-	p = @parent
-	p.gsub!('%20',' ')
-	p.gsub!('\/\/','\/')
+    @dirs = []
+    @files = []
+    p = @parent
+    p.gsub!('%20',' ')
+    p.gsub!('\/\/','\/')
     @entries = @repository.entries(path=p)
     @entries.each do |entrie|
-	r = entrie.path
-	r = r.split('/').last
-	if entrie.is_dir?
-      @dirs.push(r)
-	else
-@files.push(r)
-end
+      r = entrie.path
+      r = r.split('/').last
+      if entrie.is_dir?
+        @dirs.push(r)
+      else
+        @files.push(r)
+      end
     end
-@dir = [@dirs,@files]
-render :layout => 'reduced'
+    @dir = [@dirs,@files]
+    render :layout => 'reduced'
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
-get 'show', :to => 'show#index'
 
 RedmineApp::Application.routes.draw do
-    match 'jqueryfiletree/content', :to => 'jqueryfiletree#content', :via => [:get, :post]
+    match 'projects/:id/jqueryfiletree', :to => 'jqueryfiletree#content', :via => [:get, :post]
 end
