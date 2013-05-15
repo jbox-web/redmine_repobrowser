078dc12dfaf7219fb5d06fd20fa61ffe2e4241e9
gilbert <gilbert@erlangen.ccc.de>
2013-05-16 00:45:34 +0200

    .

--- lib/ewwiki_hook.rb
+++ lib/ewwiki_hook.rb
@@ -1,7 +1,8 @@
 class EWWikiHook < Redmine::Hook::ViewListener
-class DemoPluginHooks < Redmine::Hook::ViewListener
-    render_on :view_layouts_base_html_head,
-                :partial => 'repobrowser/toolbarmod'
-
-end
+	class DemoPluginHooks < Redmine::Hook::ViewListener
+		render_on :view_layouts_base_html_head, :partial => 'repobrowser/toolbarmod'
+	end
+	def view_layouts_base_html_head(context={})
+		stylesheet_link_tag('toolbarmod.css', :plugin => 'redmine_repobrowser')
+	end
 end
