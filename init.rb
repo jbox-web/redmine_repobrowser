b922f6b44a3262e0151177b839744af74c06d3e0
gilbert <gilbert@localhost.localdomain>
2013-05-23 10:06:17 +0200

    hopefully fixed problems with absolute path

--- init.rb
+++ init.rb
@@ -4,6 +4,6 @@ Redmine::Plugin.register :redmine_repobrowser do
   name 'Repository browser plugin'
   author 'Gilbert Johannes Martin Forkel'
   description 'Adds a button to the wiki that allows to graphically choose links to the projects repositories'
-  version '1.1.0'
+  version '1.2.0'
   url 'https://github.com/gilbertf/redmine_repobrowser'
 end
