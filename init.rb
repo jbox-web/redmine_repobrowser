2f032b7b50f89a69c6a888f2465ff9adde33c57c
gilbert <gilbert@erlangen.ccc.de>
2013-08-22 18:43:30 +0200

    version update after merge

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :redmine_repobrowser do
   name 'Repository browser plugin'
   author 'Gilbert Johannes Martin Forkel'
   description 'Adds a button to the wiki that allows to graphically choose links to the projects repositories'
-  version '1.2.0'
+  version '1.3.0'
   author_url 'https://github.com/gilbertf/redmine_repobrowser'
   url 'https://github.com/gilbertf/redmine_repobrowser'
 end
