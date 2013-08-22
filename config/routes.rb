f218581faa2e068d76bfcce330356d7d09e1f56c
root <root@dev.jbox-web.fr>
2013-08-22 17:28:40 +0200

    Indent

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
-# Plugin's routes
-# See: http://guides.rubyonrails.org/routing.html
-
-RedmineApp::Application.routes.draw do
-    match '/repobrowser/toolbarmod', :to => 'repobrowser#toolbarmod'
-    match '/repobrowser/:project_id/show', :to => 'repobrowser#show'
-    match '/repobrowser/:project_id/content', :to => 'repobrowser#content'
-end
+match '/repobrowser/toolbarmod',          :to => 'repobrowser#toolbarmod'
+match '/repobrowser/:project_id/show',    :to => 'repobrowser#show'
+match '/repobrowser/:project_id/content', :to => 'repobrowser#content'
